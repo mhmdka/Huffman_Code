b7a17ff766996ad12de12b00bd41fb09705a5b42
mokani97 <moh.kahani@gmail.com>
2018-06-01 01:47:30 +0430

    y

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,29 @@
 package com.company;
 
-import java.io.IOException;
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 	//this for testing file
-
-        File file = new File("chars.txt");
+        System.out.print("Enter Text: ");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        FileWriter fWriter;
+        BufferedWriter writer;
+        try {
+            fWriter = new FileWriter("chars.txt");
+            writer = new BufferedWriter(fWriter);
+            writer.write(text);
+            writer.newLine();
+            writer.close();
+            System.err.println("Your input of " + text.length() + " characters was saved.");
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
         HuffmanTree huffman = new HuffmanTree();
+        File file = new File("chars.txt");
         huffman.getChars(file);
     }
 }
